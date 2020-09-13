e959fcf4a8c656371690cff0a3fbb714973c327e
齐姐的自留地 <71040178+xiaoqi6666@users.noreply.github.com>
2020-09-13 16:15:18 -0400

    Create Product.java

---
+++ src/MultiThreading/ProducerConsumer/Product.java
@@ -0,0 +1,15 @@
+public class Product  {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
